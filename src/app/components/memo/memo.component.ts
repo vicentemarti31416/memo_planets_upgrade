eced4a85d0da8675ccf14097c4bcf46c1e05d322
vicentemarti31416 <vicentemarti31416@gmail.com>
2023-04-03 16:04:06 +0200

    Añadiendo funciones menores

--- src/app/components/memo/memo.component.ts
+++ src/app/components/memo/memo.component.ts
@@ -11,6 +11,8 @@ export class MemoComponent {
   @Input() cards: CardInterface[] = [];
   isClicked: boolean = false;
   selectedCards: CardInterface[] = [];
+  score: number = 0;
+  attempts: number = 0;
 
   imageDefault: CardInterface = {
     id: 6,
@@ -31,20 +33,41 @@ export class MemoComponent {
   public check(planet: CardInterface): void {
     planet.isClicked = true;
     this.selectedCards.push(planet);
+    if (this.selectedCards[0] && this.selectedCards[1]) {
+      if (this.selectedCards[0].id === this.selectedCards[1].id) {
+        this.selectedCards[0].isClicked = false;
+        this.selectedCards = []
+        this.attempts++;
+        console.log(this.attempts);
+        return;
+      }
+      if (this.selectedCards[0].isChecked) {
+        this.selectedCards = [];
+        this.attempts++;
+        console.log(this.attempts);
+        return;
+      }
+    }
     if (this.selectedCards[0] !== undefined && this.selectedCards[1] !== undefined) {
-      console.log(this.selectedCards)
       if (this.selectedCards[0].name === this.selectedCards[1].name) {
         this.selectedCards[0].isChecked = true;
         this.selectedCards[0].isClicked = false;
         this.selectedCards[1].isChecked = true;
         this.selectedCards[1].isClicked = false;
         this.selectedCards = [];
+        this.attempts++;
+        console.log(this.attempts);
+        this.score++;
       } else if (this.selectedCards[0].name !== this.selectedCards[1].name) {
-        this.selectedCards[0].isChecked = false;
-        this.selectedCards[0].isClicked = false;
-        this.selectedCards[1].isChecked = false;
-        this.selectedCards[1].isClicked = false;
-        this.selectedCards = [];
+        setTimeout (() => {
+          this.selectedCards[0].isChecked = false;
+          this.selectedCards[0].isClicked = false;
+          this.selectedCards[1].isChecked = false;
+          this.selectedCards[1].isClicked = false;
+          this.selectedCards = [];
+          this.attempts++;
+          console.log(this.attempts);
+        }, 500)
       }
     }
   }
